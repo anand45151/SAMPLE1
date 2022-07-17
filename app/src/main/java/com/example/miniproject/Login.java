be105dc5a8af50871e9e6ed71b3ccf604fd3afad
anand <ay7984267173@gmail.com>
2022-07-17 14:21:11 +0530

    xffvb

--- app/src/main/java/com/example/miniproject/Login.java
+++ app/src/main/java/com/example/miniproject/Login.java
@@ -8,9 +8,13 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
-Button newuse;
+Button newuse,dashbord;
+EditText username,password;
+DBHelper DB;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,13 +23,39 @@ Button newuse;
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN); //enable full screen
         setContentView(R.layout.activity_login);
-    newuse = findViewById(R.id.newsignup);
-    newuse.setOnClickListener(new View.OnClickListener() {
+  dashbord = findViewById(R.id.letsgo);
+      newuse = findViewById(R.id.newsignup);
+       DB = new DBHelper(this);
+        newuse.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View v) {
+
             Intent i = new Intent(Login.this,newuser.class);
             startActivity(i);
         }
     });
+        dashbord.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String user = username.getText().toString();
+                String pass = password.getText().toString();
+                if(user.equals("")||pass.equals("")){
+                    Toast.makeText(Login.this, "ENTER THE DETAILS", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Boolean checksuer = DB.checkuser(user);
+                    if(checksuer==true) {
+                        Toast.makeText(Login.this, "SIGN IS DONE", Toast.LENGTH_SHORT).show();
+                        Intent i = new Intent(Login.this,lastlog.class);
+                        startActivity(i);
+
+                    }
+                    else {
+                        Toast.makeText(Login.this, "INVALID THINGS", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/miniproject/newuser.java
+++ app/src/main/java/com/example/miniproject/newuser.java
@@ -8,9 +8,12 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class newuser extends AppCompatActivity {
 Button btn;
+EditText fname,uname,phone,email,password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,10 +24,29 @@ Button btn;
                 WindowManager.LayoutParams.FLAG_FULLSCREEN); //enable full screen
 
         setContentView(R.layout.activity_newuser);
+        fname = findViewById(R.id.full_name);
+        uname = findViewById(R.id.user_name);
+        email = findViewById(R.id.email);
+        phone = findViewById(R.id.phone);
+        password = findViewById(R.id.password);
     btn = findViewById(R.id.registerbtn);
+    DBHelper DB  = new DBHelper(this);
     btn.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View v) {
+            String user = uname.getText().toString();
+            String pas = password.getText().toString();
+            if(user.equals("")||pas.equals("")){
+                Toast.makeText(getApplicationContext(),"DEFINE THE FEILD ",Toast.LENGTH_SHORT).show();;
+            }
+            else {
+                Boolean insert = DB.insertdata(user,pas);
+                if(insert==true){
+                    Toast.makeText(getApplicationContext(),"DATA HASE BEEN DONE ",Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(getApplicationContext(),"ENTEE DATA ",Toast.LENGTH_SHORT).show();
+                }
+            }
             Intent i = new Intent(newuser.this,nextLog.class);
             startActivity(i);
         }
